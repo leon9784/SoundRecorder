da3c9e2f2c167a73d23bf9c97c3e623e56cf5f78
leon9784 <31239985+leon9784@users.noreply.github.com>
2017-12-04 23:22:15 +0800

    Initial commit

---
+++ app/src/main/java/com/zq/soundrecorder/fragments/SettingsFragment.java
@@ -0,0 +1,42 @@
+package com.zq.soundrecorder.fragments;
+
+import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+import com.zq.soundrecorder.BuildConfig;
+import com.zq.soundrecorder.MySharedPreferences;
+import com.zq.soundrecorder.R;
+import com.zq.soundrecorder.activities.SettingsActivity;
+
+
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+
+        CheckBoxPreference highQualityPref = (CheckBoxPreference) findPreference(getResources().getString(R.string.pref_high_quality_key));
+        highQualityPref.setChecked(MySharedPreferences.getPrefHighQuality(getActivity()));
+        highQualityPref.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                MySharedPreferences.setPrefHighQuality(getActivity(), (boolean) newValue);
+                return true;
+            }
+        });
+
+        Preference aboutPref = findPreference(getString(R.string.pref_about_key));
+        aboutPref.setSummary(getString(R.string.pref_about_desc, BuildConfig.VERSION_NAME));
+        aboutPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                LicensesFragment licensesFragment = new LicensesFragment();
+                licensesFragment.show(((SettingsActivity)getActivity()).getSupportFragmentManager().beginTransaction(), "dialog_licenses");
+                return true;
+            }
+        });
+    }
+}
